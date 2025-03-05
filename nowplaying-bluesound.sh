da55745b993f68438d7186fcd98b428fa3f68e7e
Mario Kozjak <kozjakm1@gmail.com>
2025-03-05 22:01:00 +0100

    huge nowplaying refactor and lyrics support
    Signed-off-by: Mario Kozjak <kozjakm1@gmail.com>

--- nowplaying-bluesound.sh
+++ nowplaying-bluesound.sh
@@ -1,36 +1,86 @@
 #!/bin/bash
 
-timeout=10
+# Configuration
+TIMEOUT=10
+BLUESOUND_HOST="bluesound.local:11000"
+TEMP_IMAGE="/tmp/nowplaying_artwork.jpg"
+SENDER="com.bluesound.bluos"
+
+# Function to extract XML tag content
+extract_tag() {
+    local response="$1"
+    local tag="$2"
+    echo "$response" | sed -n "s/.*<$tag>\(.*\)<\/$tag>.*/\1/p" | perl -MHTML::Entities -pe 'decode_entities($_);'
+}
+
+# Function to download artwork
+download_artwork() {
+    local url="$1"
+    local output_path="$2"
+    curl -s "$url" --output "$output_path"
+}
+
+# Function to perform Google search
+web_search() {
+    local search_term="$1"
+    local service="$2"
+    local encoded_search=$(python3 -c "import urllib.parse; print(urllib.parse.quote('''$search_term'''))")
+
+    case $service in
+        "google")
+            open -a Safari "https://www.google.com/search?q=$encoded_search"
+            ;;
+        "lyrics")
+            open -a Safari "https://genius.com/search?q=$encoded_search"
+            ;;
+    esac
+}
 
 # Fetch the status from the Bluesound endpoint
-response=$(curl -s "http://bluesound.local:11000/Status")
+response=$(curl -s "http://${BLUESOUND_HOST}/Status")
 
-# Extract relevant information using grep and sed/awk
-album=$(echo "$response" | sed -n 's/.*<album>\(.*\)<\/album>.*/\1/p' | perl -MHTML::Entities -pe 'decode_entities($_);')
-artist=$(echo "$response" | sed -n 's/.*<artist>\(.*\)<\/artist>.*/\1/p' | perl -MHTML::Entities -pe 'decode_entities($_);')
-title=$(echo "$response" | sed -n 's/.*<title1>\(.*\)<\/title1>.*/\1/p' | perl -MHTML::Entities -pe 'decode_entities($_);')
-image_path=$(echo "$response" | sed -n 's/.*<image>\(.*\)<\/image>.*/\1/p')
+# Extract metadata
+album=$(extract_tag "$response" "album")
+artist=$(extract_tag "$response" "artist")
+title=$(extract_tag "$response" "title1")
+image_path=$(extract_tag "$response" "image")
 
-# Decode URL components in the image path (if necessary)
-image_url="http://bluesound.local:11000$image_path"
+# Exit if no song is playing
+if [ -z "$title" ]; then
+    echo "No song currently playing."
+    exit 0
+fi
 
-# Print information (for debugging purposes)
-# echo "Album: $album"
-# echo "Artist: $artist"
-# echo "Title: $title"
-# echo "Image URL: $image_url"
+# Download artwork
+image_url="http://${BLUESOUND_HOST}${image_path}"
+download_artwork "$image_url" "$TEMP_IMAGE"
 
-imagePath="/tmp/nowplaying_artwork.jpg"
-
-# Download the album artwork
-curl -s "$image_url" --output "$imagePath"
-
-# Construct message
+# Construct notification message
 message="$artist - $album"
 
-# Send notification with alerter
-if [ -n "$imagePath" ]; then
-    alerter -title "$title" -message "$message" -timeout "$timeout" -sender com.bluesound.bluos -contentImage "$imagePath"
-else
-    alerter -title "$title" -message "$message" -timeout "$timeout" -sender com.bluesound.bluos
+# Build alerter command
+cmd="alerter -title \"$title\" -message \"$message\" -timeout \"$TIMEOUT\" -sender \"$SENDER\" -actions Research,Lyrics"
+
+if [ -f "$TEMP_IMAGE" ]; then
+    cmd+=" -contentImage \"$TEMP_IMAGE\""
+fi
+
+# Execute alerter and capture output
+OUTPUT=$(eval "$cmd")
+
+# Handle button clicks
+case $OUTPUT in
+    "Research")
+        search_term="$artist $title $album"
+        web_search "$search_term" "google"
+        ;;
+    "Lyrics")
+        search_term="$artist $title"
+        web_search "$search_term" "lyrics"
+        ;;
+esac
+
+# Cleanup
+if [ -f "$TEMP_IMAGE" ]; then
+    rm "$TEMP_IMAGE"
 fi
--- nowplaying-mac.sh
+++ nowplaying-mac.sh
@@ -1,41 +1,77 @@
 #!/bin/bash
 
-np=/opt/homebrew/bin/nowplaying-cli
+# Configuration
+NOWPLAYING_CLI=/opt/homebrew/bin/nowplaying-cli
+TIMEOUT=10
+TEMP_IMAGE="/tmp/nowplaying_artwork.jpg"
+SENDER="com.apple.finder"
 
-# Get song info from nowplaying-cli
-output=$("$np" get title artist album | perl -MHTML::Entities -pe 'decode_entities($_);')
-artwork=$("$np" get artworkData)
+# Function to decode and save artwork
+save_artwork() {
+    local artwork="$1"
+    local output_path="$2"
+    if [ -n "$artwork" ]; then
+        echo "$artwork" | base64 --decode > "$output_path"
+        echo "$output_path"
+    else
+        echo ""
+    fi
+}
 
-# Parse the lines into variables
+# Function to perform Google search
+google_search() {
+    local search_term="$1"
+    local encoded_search=$(python3 -c "import urllib.parse; print(urllib.parse.quote('''$search_term'''))")
+    open -a Safari "https://www.google.com/search?q=$encoded_search"
+}
+
+lyrics_search() {
+    local search_term="$1"
+    local encoded_search=$(python3 -c "import urllib.parse; print(urllib.parse.quote('''$search_term'''))")
+    open -a Safari "https://genius.com/search?q=$encoded_search"
+}
+
+# Get song information
+output=$("$NOWPLAYING_CLI" get title artist album | perl -MHTML::Entities -pe 'decode_entities($_);')
+artwork=$("$NOWPLAYING_CLI" get artworkData)
+
+# Parse the metadata
 title=$(echo "$output" | sed -n '1p')
 artist=$(echo "$output" | sed -n '2p')
 album=$(echo "$output" | sed -n '3p')
 
-# Print information (for debugging purposes)
-# echo "Album: $album"
-# echo "Artist: $artist"
-# echo "Title: $title"
-
-# Check if title is empty (no song playing)
+# Exit if no song is playing
 if [ -z "$title" ]; then
-  echo "No song currently playing."
-  exit 0
+    echo "No song currently playing."
+    exit 0
 fi
 
-# Construct message
+# Process artwork and construct message
 message="$artist - $album"
+imagePath=$(save_artwork "$artwork" "$TEMP_IMAGE")
 
-# Decode base64 artwork and save to temporary file
-if [ -n "$artwork" ]; then
-  echo "$artwork" | base64 --decode > /tmp/nowplaying_artwork.jpg
-  imagePath="/tmp/nowplaying_artwork.jpg"
-else
-  imagePath=""
+# Build alerter command
+cmd="alerter -title \"$title\" -message \"$message\" -timeout \"$TIMEOUT\" -sender \"$SENDER\" -actions Research,Lyrics"
+
+if [ -n "$imagePath" ] && [ -f "$imagePath" ]; then
+    cmd+=" -contentImage \"$imagePath\""
 fi
 
-# Send notification with alerter
-if [ -n "$imagePath" ]; then
-    alerter -title "$title" -message "$message" -timeout 5 -sender com.apple.finder -contentImage "$imagePath"
-else
-    alerter -title "$title" -message "$message" -timeout 5 -sender com.apple.finder
+# Execute alerter and handle response
+OUTPUT=$(eval "$cmd")
+
+case $OUTPUT in
+    "Research")
+        search_term="$artist $title $album"
+        google_search "$search_term"
+        ;;
+    "Lyrics")
+        search_term="$artist $title"
+        lyrics_search "$search_term"
+        ;;
+esac
+
+# Cleanup
+if [ -f "$TEMP_IMAGE" ]; then
+    rm "$TEMP_IMAGE"
 fi
