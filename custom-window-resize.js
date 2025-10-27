02e6f52949b0862ba840d8d7eb62d4cd235d8bcc
Mario Kozjak <kozjakm1@gmail.com>
2025-10-27 13:27:24 +0100

    add chrome to custom window resize

--- custom-window-resize.js
+++ custom-window-resize.js
@@ -19,6 +19,16 @@ async function customResize() {
         BTTActionMoveResizeName: 'Center and Resize (External)'
       }
     },
+    "Google Chrome": {
+      builtin: {
+        BTTActionMoveResizeConfig: '{"BTTOriginDisplay":0,"BTTChangeYOriginRelative":0,"BTTScreenOrigin":0,"BTTDirectionToMove":0,"BTTChangeHeightRelative":1,"BTTScreensToMove":-1,"BTTNewYAbsolute":1,"BTTNewXRelative":0,"BTTOriginToUse":0,"BTTNewWindowWidthRelative":66,"BTTChangeWindowWidth":true,"BTTNewWindowWidthAbsoluteAddition":0,"BTTNewXAbsolute":1,"BTTNewWindowHeightAbsoluteAddition":0,"BTTUseVisibleFrame":true,"BTTChangeYOrigin":1,"BTTCareForStageManager":false,"BTTChangeXOriginRelative":0,"BTTChangeWidthRelative":1,"BTTActiveOrHoveredWindow":0,"BTTNewWindowHeightRelative":96,"BTTChangeWindowHeight":true,"BTTNewYRelative":0,"BTTChangeXOrigin":1}',
+        BTTActionMoveResizeName: 'Center and Resize (Builtin)'
+      },
+      external: {
+        BTTActionMoveResizeConfig: '{"BTTOriginDisplay":0,"BTTChangeYOriginRelative":0,"BTTScreenOrigin":0,"BTTDirectionToMove":0,"BTTChangeHeightRelative":1,"BTTScreensToMove":-1,"BTTNewYAbsolute":1,"BTTNewXRelative":0,"BTTOriginToUse":0,"BTTNewWindowWidthRelative":56,"BTTChangeWindowWidth":true,"BTTNewWindowWidthAbsoluteAddition":0,"BTTNewXAbsolute":1,"BTTNewWindowHeightAbsoluteAddition":0,"BTTUseVisibleFrame":true,"BTTChangeYOrigin":1,"BTTCareForStageManager":false,"BTTChangeXOriginRelative":0,"BTTChangeWidthRelative":1,"BTTActiveOrHoveredWindow":0,"BTTNewWindowHeightRelative":96,"BTTChangeWindowHeight":true,"BTTNewYRelative":0,"BTTChangeXOrigin":1}',
+        BTTActionMoveResizeName: 'Center and Resize (External)'
+      }
+    },
     iTerm2: {
       builtin: {
         BTTActionMoveResizeConfig: '{"BTTOriginDisplay":0,"BTTChangeYOriginRelative":0,"BTTScreenOrigin":0,"BTTDirectionToMove":0,"BTTChangeHeightRelative":1,"BTTScreensToMove":-1,"BTTNewYAbsolute":1,"BTTNewXRelative":0,"BTTOriginToUse":0,"BTTNewWindowWidthRelative":60,"BTTChangeWindowWidth":true,"BTTNewWindowWidthAbsoluteAddition":0,"BTTNewXAbsolute":1,"BTTNewWindowHeightAbsoluteAddition":0,"BTTUseVisibleFrame":true,"BTTChangeYOrigin":1,"BTTCareForStageManager":false,"BTTChangeXOriginRelative":0,"BTTChangeWidthRelative":1,"BTTActiveOrHoveredWindow":0,"BTTNewWindowHeightRelative":86,"BTTChangeWindowHeight":true,"BTTNewYRelative":0,"BTTChangeXOrigin":1}',
