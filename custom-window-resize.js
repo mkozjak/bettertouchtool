06f67e7934c42af9eb15304f9061080e84bf42ab
Mario Kozjak <kozjakm1@gmail.com>
2026-02-23 23:25:40 +0100

    add Terminal.app to custom window resize js
    Signed-off-by: Mario Kozjak <kozjakm1@gmail.com>

--- custom-window-resize.js
+++ custom-window-resize.js
@@ -39,6 +39,16 @@ async function customResize() {
         BTTActionMoveResizeName: 'Center and Resize (External)'
       }
     },
+    Terminal: {
+      builtin: {
+        BTTActionMoveResizeConfig: '{"BTTOriginDisplay":0,"BTTChangeYOriginRelative":0,"BTTScreenOrigin":0,"BTTDirectionToMove":0,"BTTChangeHeightRelative":1,"BTTScreensToMove":-1,"BTTNewYAbsolute":1,"BTTNewXRelative":0,"BTTOriginToUse":0,"BTTNewWindowWidthRelative":60,"BTTChangeWindowWidth":true,"BTTNewWindowWidthAbsoluteAddition":0,"BTTNewXAbsolute":1,"BTTNewWindowHeightAbsoluteAddition":0,"BTTUseVisibleFrame":true,"BTTChangeYOrigin":1,"BTTCareForStageManager":false,"BTTChangeXOriginRelative":0,"BTTChangeWidthRelative":1,"BTTActiveOrHoveredWindow":0,"BTTNewWindowHeightRelative":96,"BTTChangeWindowHeight":true,"BTTNewYRelative":0,"BTTChangeXOrigin":1}',
+        BTTActionMoveResizeName: 'Center and Resize (Builtin)'
+      },
+      external: {
+        BTTActionMoveResizeConfig: '{"BTTOriginDisplay":0,"BTTChangeYOriginRelative":0,"BTTScreenOrigin":0,"BTTDirectionToMove":0,"BTTChangeHeightRelative":1,"BTTScreensToMove":-1,"BTTNewYAbsolute":1,"BTTNewXRelative":0,"BTTOriginToUse":0,"BTTNewWindowWidthRelative":50,"BTTChangeWindowWidth":true,"BTTNewWindowWidthAbsoluteAddition":0,"BTTNewXAbsolute":1,"BTTNewWindowHeightAbsoluteAddition":0,"BTTUseVisibleFrame":true,"BTTChangeYOrigin":1,"BTTCareForStageManager":false,"BTTChangeXOriginRelative":0,"BTTChangeWidthRelative":1,"BTTActiveOrHoveredWindow":0,"BTTNewWindowHeightRelative":86,"BTTChangeWindowHeight":true,"BTTNewYRelative":0,"BTTChangeXOrigin":1}',
+        BTTActionMoveResizeName: 'Center and Resize (External)'
+      }
+    },
     Mail: {
       builtin: {
         BTTActionMoveResizeConfig: '{"BTTOriginDisplay":0,"BTTChangeYOriginRelative":0,"BTTScreenOrigin":0,"BTTDirectionToMove":0,"BTTChangeHeightRelative":1,"BTTScreensToMove":-1,"BTTNewYAbsolute":1,"BTTNewXRelative":0,"BTTOriginToUse":0,"BTTNewWindowWidthRelative":70,"BTTChangeWindowWidth":true,"BTTNewWindowWidthAbsoluteAddition":0,"BTTNewXAbsolute":1,"BTTNewWindowHeightAbsoluteAddition":0,"BTTUseVisibleFrame":true,"BTTChangeYOrigin":1,"BTTCareForStageManager":false,"BTTChangeXOriginRelative":0,"BTTChangeWidthRelative":1,"BTTActiveOrHoveredWindow":0,"BTTNewWindowHeightRelative":84,"BTTChangeWindowHeight":true,"BTTNewYRelative":0,"BTTChangeXOrigin":1}',
